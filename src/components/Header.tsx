52e1fd86f8b7c2d005bfd07d2d25545f1bb57807
enbraining <me@xolving.com>
2024-02-28 04:41:57 +0000

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,33 @@
 
 import Image from "next/image";
 import styled from "styled-components";
+import xolving from "@/public/xolving.svg"
 
 const StyledHeader = styled.header`
   && {
     display: flex;
     border-bottom-width: 1px;
+    padding: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+    align-items: center;
+    column-gap: 20px;
+    box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
   }
 
   .menus {
     display: flex;
     column-gap: 10px;
-    margin: 18px;
   }
 `;
 
 export default function Header(){
     return (
         <StyledHeader>
+            <a href="/"><Image src={xolving} alt="xolving" width={20} height={20} /></a>
             <div className="menus">
-                <a href="/about"><p>About</p></a>
-                <a href="/discord"><p>Discord</p></a>
+                <a href="/about"><p>about</p></a>
+                <a href="/discord"><p>discord</p></a>
             </div>
         </StyledHeader>
     )
