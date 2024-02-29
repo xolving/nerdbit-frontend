59d9d893fb266090f637f0e069b0c9c4c79076fb
enbraining <me@xolving.com>
2024-02-29 08:49:52 +0000

    update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import { Title } from "@/components/Title";
-import styled from "styled-components";
-
-
 
 export default function Home() {
   return (
@@ -9,7 +6,7 @@ export default function Home() {
       <div className="text-center pt-10 font-light">
         <Title>xolving</Title>
         <p className="pt-3 text-xl mb-5">개발자에 의한, 개발자를 위한</p>
-        <a href="https://discord.gg/36tCw3kYuJ" className="bg-gradient-to-r from-red-500 to-orange-500 text-slate-100 font-bold py-2 px-5 rounded-lg">디스코드</a>
+        <a href="https://discord.gg/36tCw3kYuJ" className="bg-gradient-to-r from-red-500 to-orange-500 text-white font-bold py-2 px-7 rounded-lg">디스코드</a>
       </div>
     </main>
   );
