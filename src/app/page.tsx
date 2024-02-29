f8bcd609ef84a18cd7d203375d71f64fe7e5c7c6
enbraining <me@xolving.com>
2024-02-29 02:06:32 +0000

    add discord button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <main>
       <div className="text-center pt-10 font-light">
         <Title>xolving</Title>
-        <p className="pt-3 text-xl">개발자에 의한, 개발자를 위한</p>
-        <a href="https://discord.gg/36tCw3kYuJ" className="bg-gradient-to-r from-red-500 to-orange-500 text-slate-100 mt-5 font-bold py-2 px-5 rounded-lg">디스코드</a>
+        <p className="pt-3 text-xl mb-5">개발자에 의한, 개발자를 위한</p>
+        <a href="https://discord.gg/36tCw3kYuJ" className="bg-gradient-to-r from-red-500 to-orange-500 text-slate-100 font-bold py-2 px-5 rounded-lg">디스코드</a>
       </div>
     </main>
   );
