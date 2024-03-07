72f15e090d1343c0a2ab219c1dd430a8d7fc6dcb
enbraining <me@xolving.com>
2024-03-07 21:11:07 +0900

    remove footer dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local'
 import "./globals.css";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const inter = localFont({ 
   src: '../public/fonts/PretendardVariable.woff2',
